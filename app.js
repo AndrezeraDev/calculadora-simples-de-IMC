22279b3c6051c7515587816e55395f06ded56e4b
André Luis de Souza <andrezera.dev@gmail.com>
2024-11-26 22:37:47 -0300

    Adição dos arquivos iniciais

---
+++ app.js
@@ -0,0 +1,33 @@
+const nome = document.getElementById('name');
+var altura = document.getElementById('height');
+var peso = document.getElementById('weight');
+const calcularBtn = document.getElementById('calcular');
+const resultado = document.getElementById('results');
+
+calcularBtn.addEventListener("click", function (e) {
+    e.preventDefault()
+
+    calcular();
+})
+
+function calcular() {
+    
+    var imc = peso.value / (altura.value * altura.value);
+    var imcFormatado = imc.toFixed(2);
+    console.log(imcFormatado);
+
+    if (peso.value == "" || altura.value == "") {
+        alert("Preencha todos os campos");
+        return;
+    }
+
+    if(imcFormatado < 18.5){
+        resultado.innerText =  `${nome.value} Seu IMC é de ${imcFormatado} Você está abaixo do peso`
+    }else if(imcFormatado <= 25){
+        resultado.innerText = `${nome.value} Seu IMC é de ${imcFormatado} Vocé está no peso ideal`
+    }else if(imcFormatado <= 30){
+        resultado.innerText = `${nome.value} Seu IMC é de ${imcFormatado} Vocé está com sobrepeso`
+    }else if(imcFormatado <= 40){
+        resultado.innerText = `${nome.value} Seu IMC é de ${imcFormatado} Vocé está com obesidade`
+    }
+}
